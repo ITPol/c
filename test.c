81f61a00d0204e40c0053309179a35d31f7e6996
c0daman <c0daman@hotmail.com>
2012-06-12 22:41:04 +0300

    updated test.c file :)

--- test.c
+++ test.c
@@ -2,7 +2,9 @@
 
 main()
 {
+  char* fiskaya = "\033[22;31m Hello yes ,ı am learning git system :)\n";
   char* str = "Merhaba my foreign friend, testing editing github files.\n";
 printf("Merhaba Dünya , test github.\n");
 printf("%s",str);
+printf("%s",fiskaya);
 }
