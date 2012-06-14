e0edd3198d8d3efc58f4404b7ea08928f6169ac8
ITPol <polymath44@live.com>
2012-06-14 17:17:53 +0300

    Update, found your console color list and added light green :d

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ main()
   char* str = "Merhaba my foreign friend, testing editing github files.\n";
   
   // Sorry for the long text, don't know how to make a new line without breaking the string (in java you can use the + operator)
-  char* itpol_a = "Glad to meet you :).\n So I am pretty new to github and c, how do you make those colors like \033[22;31m\n Is there a list for those colors?\n I did compile this code and saw your red text haha that is very nice :)\n";
+  char* itpol_a = "\033[01;32m Glad to meet you :).\n So I am pretty new to github and c, how do you make those colors like \033[22;31m\n Is there a list for those colors?\n I did compile this code and saw your red text haha that is very nice :)\n";
   
   /** Conversation */
   printf("Merhaba Dünya , test github.\n");
